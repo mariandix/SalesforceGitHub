8409dd320fd2c1b9a05d9ab4af36d9628fc9be38
hiteshsalesforce <31140470+hiteshsalesforce@users.noreply.github.com>
2017-10-17 09:17:11 +0200

    Update config.inc.php

--- app/config/config.inc.php
+++ app/config/config.inc.php
@@ -1,6 +1,6 @@
 <?php 
 
-// rqa
+// prod
 
 define("CLIENT_ID", "3MVG9HxRZv05HarQxzosOFnfW1uJ7p6TbNw1bhkKM81UhwoTyjYUGsluFvSbcz9ORHdY9t71ABcjfm98ujWpp");
 define("CLIENT_SECRET", "2555485261992489298");
@@ -13,35 +13,9 @@ define('BUTTON_ID', '5730Y0000008flT');
 define('ORG_ID', '00D0Y000001Xopw');
 define('LIVEAGENT_REST_URL', 'https://d.la1-c2-frf.salesforceliveagent.com/chat/rest/');
 
-// rqa
-/*
-define("CLIENT_ID", "3MVG9od6vNol.eBgxIiNM0JqZp_YuXM5fxbYJ5_UJvbU78w98EESnSRTODTMGhM51aECTJNya0ok0flaqDuzi");
-define("CLIENT_SECRET", "3577859520326194529");
-define("LOGIN_URI", "https://test.salesforce.com");  
-define('USERNAME', 'hmalhotra@meinservice.ext.dpdhl.rqa');
-define('PASSWORD', 'P@ss0102VEIoEkXNzTpgM9fz2GRupAya');
 
-define('DEPLOYMENT_ID', '572260000008OQT');
-define('BUTTON_ID', '573260000008Oh0');
-define('ORG_ID', '00D260000009FVj');
-
-*/
-// dev
-/*define("CLIENT_ID", "3MVG954MqIw6FnnM4PE4C_n5rf0LaljT_OSDyZOy7ZnuYv.ij6XWqYIxoW9ttOqeWiQ_K4sWuk9xCTbxak78b");
-define("CLIENT_SECRET", "4557138230826258012");
-define("LOGIN_URI", "https://test.salesforce.com");  
-define('USERNAME', 'hmalhotra@dpdhl.chatbot.dev');
-define('PASSWORD', 'P@ss0102nHI6woYcRV7ZfFknsYQJsxVLQ');
-
-define('DEPLOYMENT_ID', '5728E0000008OP1');
-define('BUTTON_ID', '5738E0000008OSU');
-define('ORG_ID', '00D8E000000D20l');
-*/
 
 define('COGNESYS_URL', 'https://lhZepTho1M0xnFVznOsT:GRi78QOuyeHpFh0bC5BA@api.cognesys.de:5679/prototype');
-//define('COGNESYS_URL', 'https://trh78b4563vg456456:nuk3258nxq2q3c524@api.cognesys.de:5679/prototype');
-//define('COGNESYS_URL', 'https://nuk3258nxq2q3c524:trh78b4563vg456456@78.137.97.103:4444/prototype');
-
 
 
 define('LIVEAGENT_CHECK_COUNT', 8);
