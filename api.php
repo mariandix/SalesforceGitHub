91438b05734d4d7462d14802161dbca3aec9dead
tkaehler <th.kaehler@web.de>
2017-11-15 11:43:51 +0100

    fix

--- api.php
+++ api.php
@@ -148,12 +148,10 @@ $params = '{
 "chatBotSummary":"' . (isset($data->summary)?addslashes(implode(',',$data->summary)):"") . '",
 "tonality": "' . (isset($data->tonality) ? $data->tonality : "") . '",
 "chatStartTime": "' . (isset($data->startTime) ? $data->startTime : "2017-09-04T13:20:49.717+02:00") . '",
-"chatEndTime": "' . (isset($data->endTime) ? $data->endTime : "2017-09-04T13:25:01.975+02:00") . '"
+"chatEndTime": "' . (isset($data->endTime) ? $data->endTime : "2017-09-04T13:25:01.975+02:00") . '", 
+"jsonStringBody": "' . (strlen(addslashes(implode(',', $_SESSION['chatbot']))) > 32000 ? "{}" : addslashes(implode(',', $_SESSION['chatbot']))) . '" 
 }
 }';
-
-// , 
-//"jsonStringBody": "' . addslashes(implode(',', $_SESSION['chatbot'])) . '" 
 				
 				$con->setPostfields($params);
 
@@ -863,7 +861,8 @@ $params = '{
 "callbackInfo":"' . (isset($_POST['callback']) ? $_POST['callback']: "") . '",
 "chatStatus":"Aborted",
 "chatBotSummary":"' . (isset($_POST['summary']) ? $_POST['summary']: "") . '",
-"tonality": "' . (isset($responseStopData->tonality) ? $responseStopData->tonality : "") . '"
+"tonality": "' . (isset($responseStopData->tonality) ? $responseStopData->tonality : "") . '", 
+"jsonStringBody": "' . (strlen(addslashes(implode(',', $_SESSION['chatbot']))) > 32000 ? "{}" : addslashes(implode(',', $_SESSION['chatbot']))) . '" 
 }
 }';
 				
